6d99930f0c066daf554acecdf7caafbac20f65e2
Dimitris Tsironis <tsironis.dimitris@gmail.com>
2013-11-23 01:20:40 +0200

    Changes algorithm, now calculates timedeltas for each trail compared to the previous

--- stats.py
+++ stats.py
@@ -1,11 +1,23 @@
 import sys
-import time, datetime
+from datetime import datetime, date, time
+
+def avg_time(datetimes):
+  previous = None
+  deltas = []
+  for dt in datetimes:
+    if (previous):
+      d = dt - previous
+      deltas.append(divmod(d.days * 86400 + d.seconds, 60))
+    previous = dt
+
+  return deltas
+
 with open(".trails","r") as theFile:
   fmt   = '%d %B %Y %H:%M:%S'
   times = []
   for line in theFile:
     data = line.split(' ->')[0]
-    ts = time.strptime(data, fmt)
+    ts = datetime.strptime(data, fmt)
     times.append(ts)
 
-  print(times)
+  print(avg_time(times))
